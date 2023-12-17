9b4d8879b6de37239f26af794a1aff8daa181478
LieutenantCobretti1998 <djamil5099@gmail.com>
2023-12-17 14:20:39 +0100

    Improve the visibility of the menu

--- Display/themes_and_fonts.py
+++ Display/themes_and_fonts.py
@@ -1,15 +1,14 @@
 import pygame_menu
 
-
-main_font = pygame_menu.font.FONT_DIGITAL
+# main_font = pygame_menu.font.FONT_DIGITAL Not used right now
 pause_font = pygame_menu.font.FONT_FIRACODE_BOLD_ITALIC
 
-menu_transpareny_theme = pygame_menu.Theme(background_color=(0, 0, 0, 2),
+menu_transparency_theme = pygame_menu.Theme(background_color=(0, 0, 0, 2),
                                            title_background_color=(75, 123, 186, 204),
-                                           title_font_color=(180, 0, 129, 2),
-                                           widget_font_color=(81, 12, 177, 2),
+                                           title_font_color=(180, 0, 255, 2),
+                                           widget_font_color=(0, 174, 177, 2),
                                            widget_padding=5,
-                                           widget_font=main_font)
+                                           widget_font=pause_font)
 
 
 menu_black_background = pygame_menu.Theme(background_color=(0, 0, 0, 70),
